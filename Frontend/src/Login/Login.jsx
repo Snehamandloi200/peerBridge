9e216dd0fabd204ea1bdb12cd96384e7448f3d8b
Renuka <renukakushwah60@gmail.com>
2025-12-12 23:23:15 +0530

    Done

--- Frontend/src/Login/Login.jsx
+++ Frontend/src/Login/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
-import "./login.css";
+import "./Login.css";
 
 function Login() {
   const navigate = useNavigate();
