11b228e4e51cd07c8ca8eff89b73c3e8ab8d363d
Renuka <renukakushwah60@gmail.com>
2025-12-13 16:04:39 +0530

    Final

--- Backend/app.js
+++ Backend/app.js
@@ -32,7 +32,7 @@ app.use(bodyParser.json());
 app.use(methodOverride("_method"));
 app.use(
   cors({
-    origin: "http://localhost:5173", 
+    origin: "https://peerbridge-1-c2m4.onrender.com", 
   })
 );
 
